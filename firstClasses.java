283daadce2489d55df416bbdf5d9f8ee3cdf95d0
rmasey <rmasey@hotmail.com>
2016-08-23 12:57:01 +0100

    first commit

---
+++ firstClasses.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class firstClasses 
+{ 
+    public static void main(String[] args) 
+    { 
+
+        Scanner input = new Scanner(System.in);   //need to create Scanner object to take input, object name is input
+
+        System.out.println("Enter your name" );   
+        String yourName = input.nextLine();        // take string input
+
+        System.out.println("Enter your age" );
+        int yourAge = input.nextInt();             // take int input
+
+        System.out.println("Enter float:");
+        float yourHeight = input.nextFloat();    // take float input
+
+        System.out.println("Hello " + yourName);   //prints name, cursor put on new line
+
+    } 
+} 
---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hello 
+{ 
+    public static void main(String[] args) 
+    { 
+
+        Scanner input = new Scanner(System.in);   //need to create Scanner object to take input, object name is input
+
+        System.out.println("Hello y'all" );   
+        
+    } 
+} 
